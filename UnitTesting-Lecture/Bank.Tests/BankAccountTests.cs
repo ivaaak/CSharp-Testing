da45e9b9b8935daf784da3840e351af13452d0d8
Ivo <43663336+ivaaak@users.noreply.github.com>
2022-05-23 00:04:55 +0300

    added unit testing
    demo project showing unit testing from a softuni lecture

---
+++ UnitTesting-Lecture/Bank.Tests/BankAccountTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Bank.Tests
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        private decimal amount;
+        private string name;
+        private BankAccount bankAccount;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.name = "Pesho";
+            this.amount = 5;
+            this.bankAccount = new BankAccount(this.amount, this.name);
+        }
+
+        [Test]
+        public void AccountInitalizedWithPositiveValue_Amount_ShouldBeChanged()
+        {
+            Assert.That(bankAccount.Amount, Is.EqualTo(amount));          
+        }
+
+        [Test]
+        public void AccountDepositIsMade_ShouldChangeAmount()
+        {
+            bankAccount.Deposit(amount);
+
+            Assert.That(bankAccount.Amount, Is.EqualTo(amount * 2));
+        }
+    }
+}
---
+++ UnitTesting-Lecture/Bank/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //main needed for the app to build
+        }
+    }
+}
