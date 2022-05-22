095667d04e2de416541d4f0ebb5b09def5eff5ee
Ivo <43663336+ivaaak@users.noreply.github.com>
2022-05-22 14:07:07 +0300

    added a console app - makes data access easier

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = ContainerConfig.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+                app.Run();
+            }
+        }
+    }
+}
